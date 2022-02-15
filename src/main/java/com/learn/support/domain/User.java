5b5609c833848e55fffcefbdc2055e65f84ac915
mrdbrg <rdg.brg@gmail.com>
2022-02-15 20:53:51 -0300

    map user class to database

--- src/main/java/com/learn/support/domain/User.java
+++ src/main/java/com/learn/support/domain/User.java
@@ -3,8 +3,23 @@ package com.learn.support.domain;
 import java.io.Serializable;
 import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// mapping class as a table
+@Entity	// mark class as a JPA Entity
 public class User implements Serializable {
 	
+	//	once the application runs, JPA will pick up on the annotations
+	//	and will map the User class into a database with all of the 
+	//	fields below as the database row.
+	
+	@Id	// The @Id annotation marks a field as a primary key field. When a primary key field is defined the primary key value is automatically injected into that field by ObjectDB.
+	@GeneratedValue(strategy = GenerationType.AUTO)	// The @GeneratedValue annotation specifies that the primary key is automatically allocated by ObjectDB. There are different value generation strategies.
+	@Column(nullable = false, updatable = false)
 	private Long id;
 	private String userId;
 	private String firstName;
@@ -19,6 +34,120 @@ public class User implements Serializable {
 	private String[] roles; // will hold the role of users - ROLE_ADMIN{ read, edit, delete, update, create }
 	private String[] authorities; // { read, edit, delete, update, create }
 	private boolean isActive;
-	private boolean isNotLocked;	
+	private boolean isNotLocked;
+	
+	public User() {}
+	
+	public User(Long id, String userId, String firstName, String lastName, String username, String password,
+			String email, String imageUrl, Date lastLoginDate, Date lastLoginDateDisplay, Date joinDate, String[] roles,
+			String[] authorities, boolean isActive, boolean isNotLocked) {
+//		super();
+		this.id = id;
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.imageUrl = imageUrl;
+		this.lastLoginDate = lastLoginDate;
+		this.lastLoginDateDisplay = lastLoginDateDisplay;
+		this.joinDate = joinDate;
+		this.roles = roles;
+		this.authorities = authorities;
+		this.isActive = isActive;
+		this.isNotLocked = isNotLocked;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public Date getLastLoginDate() {
+		return lastLoginDate;
+	}
+	public void setLastLoginDate(Date lastLoginDate) {
+		this.lastLoginDate = lastLoginDate;
+	}
+	public Date getLastLoginDateDisplay() {
+		return lastLoginDateDisplay;
+	}
+	public void setLastLoginDateDisplay(Date lastLoginDateDisplay) {
+		this.lastLoginDateDisplay = lastLoginDateDisplay;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	public String[] getRoles() {
+		return roles;
+	}
+	public void setRoles(String[] roles) {
+		this.roles = roles;
+	}
+	public String[] getAuthorities() {
+		return authorities;
+	}
+	public void setAuthorities(String[] authorities) {
+		this.authorities = authorities;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public boolean isNotLocked() {
+		return isNotLocked;
+	}
+	public void setNotLocked(boolean isNotLocked) {
+		this.isNotLocked = isNotLocked;
+	}	
 	
 }
