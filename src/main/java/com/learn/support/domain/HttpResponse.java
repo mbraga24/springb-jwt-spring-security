96db2a4ce44ce9b9aed64bf84aca69b0afba57b8
mrdbrg <rdg.brg@gmail.com>
2022-02-16 13:47:51 -0300

    user httpResponse

---
+++ src/main/java/com/learn/support/domain/HttpResponse.java
@@ -0,0 +1,18 @@
+package com.learn.support.domain;
+
+import org.springframework.http.HttpStatus;
+
+public class HttpResponse {
+	
+	@SuppressWarnings("unused")
+	private int httpStatusCode;
+	@SuppressWarnings("unused")
+	private HttpStatus httpStatus;
+	@SuppressWarnings("unused")
+	private String reason;
+	@SuppressWarnings("unused")
+	private String message;
+	
+	
+	
+}
