8ed410d264c8454198715436edb0d85f60029ebe
mrdbrg <rdg.brg@gmail.com>
2022-02-16 16:25:45 -0300

    UserPrincipal for spring security

---
+++ src/main/java/com/learn/support/domain/UserPrincipal.java
@@ -0,0 +1,72 @@
+package com.learn.support.domain;
+
+import static org.junit.jupiter.api.DynamicTest.stream;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+// This domain is where I can map isActive, authorities and isNotLocked from 
+// the User class to the Spring user in the security context.
+
+// UserDetails comes from Spring Security - it provides core user information
+// from a security perspective. The UserPrincipal is the user that Spring Security
+// is going to need to do all the work. Because UserDetails is an interface it 
+// already has boilerplate methods that work with Spring Security. 
+// Some I will need to pass the actual the value from the User class so Spring
+// Security can map those values and determine if they're correct or not. Other 
+// methods I might need to set a generic value to guarantee the application will 
+// run.
+
+@SuppressWarnings("serial")
+public class UserPrincipal implements UserDetails {
+	private User user;
+	
+	public UserPrincipal(User user) {
+		super();
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return stream(this.user.getAuthorities()).map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getPassword() {
+		return this.user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.user.getUsername();
+	}
+
+	// I didn't define this field so I don't have this field in 
+	// the user class. When it's set to "true", the app won't fail 
+	// when tries to log in
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return this.user.isNotLocked();
+	}
+
+	// I didn't define this field so I don't have this field in 
+	// the user class. When it's set to "true", the app won't fail 
+	// when tries to log in
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return this.user.isActive();
+	}	
+}
