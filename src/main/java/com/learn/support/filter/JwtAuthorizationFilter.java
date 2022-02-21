141f1232023b83cac706017f3ec941928d1c9db9
mrdbrg <rdg.brg@gmail.com>
2022-02-21 18:11:19 -0500

    add @Component annotation bean

--- src/main/java/com/learn/support/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/learn/support/filter/JwtAuthorizationFilter.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.learn.support.constant.SecurityConstant;
