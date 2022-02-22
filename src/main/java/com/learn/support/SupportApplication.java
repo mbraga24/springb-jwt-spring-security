cf5211c62a9cf148342e5169061b1dbfdeeb97a0
mrdbrg <rdg.brg@gmail.com>
2022-02-22 15:53:38 -0500

    BCryptPasswordEncoder bean

--- src/main/java/com/learn/support/SupportApplication.java
+++ src/main/java/com/learn/support/SupportApplication.java
@@ -2,6 +2,8 @@ package com.learn.support;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SupportApplication {
@@ -10,4 +12,8 @@ public class SupportApplication {
 		SpringApplication.run(SupportApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
