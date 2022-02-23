742fbae253f8d2859787be693b8a2663b94bf23d
mrdbrg <rdg.brg@gmail.com>
2022-02-23 16:15:51 -0500

    override default getErrorPath method to send custom message on http response

--- src/main/java/com/learn/support/exception/model/ExceptionHandling.java
+++ src/main/java/com/learn/support/exception/model/ExceptionHandling.java
@@ -7,6 +7,7 @@ import javax.persistence.NoResultException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.auth0.jwt.exceptions.TokenExpiredException;
@@ -27,7 +29,7 @@ import com.learn.support.domain.HttpResponse;
 //============================================================================================
 
 @RestControllerAdvice
-public class ExceptionHandling {
+public class ExceptionHandling implements ErrorController {
 	
 	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 	private static final String ACCOUNT_LOCKED = "Your account has been locked.";
@@ -37,7 +39,8 @@ public class ExceptionHandling {
 	private static final String ACCOUNT_DISABLED = "Your account has been disabled.";
 	private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file.";
 	private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission.";
-
+	private static final String ERROR_PATH = "/error"; // Preferred strategy to a 404 not found error path
+	
 	@ExceptionHandler(DisabledException.class)
 	public ResponseEntity<HttpResponse> accountDisabledException() {
 		return createHttpResponse(HttpStatus.BAD_REQUEST, ACCOUNT_DISABLED);
@@ -112,6 +115,16 @@ public class ExceptionHandling {
 		httpStatus.getReasonPhrase().toUpperCase(), message.toUpperCase());
 		return new ResponseEntity<>(httpResponse, httpStatus);
 	}
-
+	
+	// Preferred strategy to a 404 not found error path
+	@RequestMapping(ERROR_PATH)
+	public ResponseEntity<HttpResponse> notFound404() {
+		return createHttpResponse(HttpStatus.NOT_FOUND, "No mapping for this URL.");
+	}
+	
+	// Override method that returns the path to the error. The method that is called to show the page.
+	public String getErrorPath() {
+		return ERROR_PATH;
+	}
 }
 
