ca359d1f6803bf62be0744f4e185c3194533f409
mrdbrg <rdg.brg@gmail.com>
2022-02-23 15:04:28 -0500

    extends ExceptionHandling / testing exceptions

--- src/main/java/com/learn/support/resource/UserResource.java
+++ src/main/java/com/learn/support/resource/UserResource.java
@@ -4,13 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.learn.support.exception.model.ExceptionHandling;
+import com.learn.support.exception.model.UserNotFoundException;
+
 @RestController
 @RequestMapping(value = "/user")
-public class UserResource {
+public class UserResource extends ExceptionHandling {
 
 	@GetMapping("/home")
-	public String showUser() {
-		return "Application Works";
+	public String showUser() throws UserNotFoundException {
+		
+		throw new UserNotFoundException("User not found.");
+//		return "Application Works";
 	}
 	
 }
