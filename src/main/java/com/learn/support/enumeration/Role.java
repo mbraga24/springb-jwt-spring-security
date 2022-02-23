b4da1801e9db7409b1ebb41e4e20b925105331ef
mrdbrg <rdg.brg@gmail.com>
2022-02-23 17:31:16 -0500

    enum with roles - accessible by application

---
+++ src/main/java/com/learn/support/enumeration/Role.java
@@ -0,0 +1,28 @@
+package com.learn.support.enumeration;
+
+import com.learn.support.constant.Authority;
+
+//========================================> Role <========================================
+// This enum will be available to the rest of the application. When using one of the enums 
+// and getAuthorities() is called, it will return the authorities associated to that role.
+// This way if the a role is passed to a user, I can get the authorities for that user.
+//========================================================================================
+
+public enum Role {
+	
+	ROLE_USER(Authority.USER_AUTHORITIES),
+	ROLE_HR(Authority.HR_AUTHORITIES),
+	ROLE_MANAGER(Authority.MANAGER_AUTHORITIES),
+	ROLE_ADMIN(Authority.ADMIN_AUTHORITIES),
+	ROLE_SUPER_ADMIN(Authority.SUPER_ADMIN_AUTHORITIES);
+
+	private String[] authorities;
+
+	Role(String... authorities) {
+		this.authorities = authorities;
+	}
+	
+	public String[] getAuthorities() {
+		return authorities;
+	}
+}
