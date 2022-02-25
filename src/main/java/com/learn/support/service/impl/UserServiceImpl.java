82732ec677179b9681772b7c883f7a45d5a42196
mrdbrg <rdg.brg@gmail.com>
2022-02-25 15:17:36 -0500

    add register method / add comments

--- src/main/java/com/learn/support/service/impl/UserServiceImpl.java
+++ src/main/java/com/learn/support/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.learn.support.service.impl;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +14,18 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+//import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.learn.support.domain.User;
 import com.learn.support.domain.UserPrincipal;
+import com.learn.support.enumeration.Role;
+import com.learn.support.exception.model.EmailExistException;
+import com.learn.support.exception.model.UserNotFoundException;
+import com.learn.support.exception.model.UsernameExistException;
 import com.learn.support.repository.UserRepository;
 import com.learn.support.service.UserService;
-
 
 @Service
 @Transactional // manage propagation whenever the service is dealing more transactions
@@ -26,10 +34,12 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
 	private Logger LOGGER = LoggerFactory.getLogger(getClass()); // UserServiceImpl.class()
 	private UserRepository userRepository;
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
-	public UserServiceImpl(UserRepository userRepository) {
+	public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 	
 	// The loadUserByUsername method gets called whenever Spring Security is trying to check the
@@ -48,6 +58,93 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 			return userPrincipal;
 		}
 		
+	}
+
+	@Override
+	public User register(String firstName, String lastName, String username, String email) throws UsernameExistException, UserNotFoundException, EmailExistException {
+		validateNewUsernameAndEmail(StringUtils.EMPTY, username, email);
+		User user = new User();
+		user.setUserId(generateUserId());
+		String password = generatePassword();
+		String encodedPassword = encodePassword(password);
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setUsername(username);
+		user.setEmail(email);
+		user.setJoinDate(new Date());
+		user.setPassword(encodedPassword);
+		user.setActive(true);
+		user.setNotLocked(true);
+		user.setRole(Role.ROLE_USER.name()); // setRole() takes a String. ROLE_USER is an enum and name() method will convert the enum into a String.
+		user.setAuthorities(Role.ROLE_USER.getAuthorities());
+		// user.setProfileImageUrl(getTemporaryProfileImageUrl());
+		userRepository.save(user); // call userRepository to save the user in the database
+		LOGGER.info("New user password: " + password);
+		return user;
+	}
+
+	//	private Object getTemporaryProfileImageUrl() {
+	//		// ServletUriComponentsBuilder will return whatever the URL is for the service.
+	//		// e.g.: if the application is deployed to Google.com the based URL will be 
+	//		// www.google.com/...
+	//		return ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/image/profile/temp").toUriString();
+	//	}
+
+	private String encodePassword(String password) {
+		return passwordEncoder.encode(password);
+	}
+
+	private String generatePassword() {
+		return RandomStringUtils.randomAlphanumeric(10);
+	}
+
+	private String generateUserId() {
+		return RandomStringUtils.randomNumeric(10);
+	}
+
+	// This method will be very generic to be applicable for when someone is either creating a new acccount 
+	// or updating their account.
+	private User validateNewUsernameAndEmail(String currentUsername, String newUsername, String email) throws UsernameExistException, UserNotFoundException, EmailExistException {
+		if (StringUtils.isNotBlank(currentUsername)) {
+			User currentUser = findUserByUsername(currentUsername);
+			if (currentUser == null) {
+				throw new UserNotFoundException("No user found by username: " + currentUsername);
+			}
+			User userByNewUsername = findUserByUsername(newUsername);
+			if (userByNewUsername != null && !currentUser.getId().equals(userByNewUsername.getId())) {
+				throw new UsernameExistException("Username already exists");
+			}
+			User userByNewEmail = findUserByEmail(email);
+			if (userByNewEmail != null && !currentUser.getId().equals(userByNewEmail.getId())) {
+				throw new EmailExistException("Email already exist");
+			}
+			return currentUser;
+		} else {
+			User userByUsername = findUserByUsername(newUsername);
+			if (userByUsername != null) {
+				throw new UsernameExistException("Username already exists");
+			}
+			User userByEmail = findUserByEmail(email);
+			if (userByEmail != null) {
+				throw new EmailExistException("Email already exist");
+			}
+			return null;
+		}
+	}
+
+	@Override
+	public List<User> getUsers() {
+		return null;
+	}
+
+	@Override
+	public User findUserByUsername(String username) {
+		return null;
+	}
+
+	@Override
+	public User findUserByEmail(String email) {
+		return null;
 	}
 
 }
