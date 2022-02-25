0480bfcfce8466ed7d8f3ef5e94adadd6640e30d
mrdbrg <rdg.brg@gmail.com>
2022-02-25 15:14:54 -0500

    throw exceptions

--- src/main/java/com/learn/support/service/UserService.java
+++ src/main/java/com/learn/support/service/UserService.java
@@ -1,10 +1,15 @@
 package com.learn.support.service;
 
+import java.util.List;
+
 import com.learn.support.domain.User;
+import com.learn.support.exception.model.EmailExistException;
+import com.learn.support.exception.model.UserNotFoundException;
+import com.learn.support.exception.model.UsernameExistException;
 
 public interface UserService {
 	
-	User register(String firstName, String lastName, String username, String email);
+	User register(String firstName, String lastName, String username, String email) throws UsernameExistException, UserNotFoundException, EmailExistException;
 	
 	List<User> getUsers();
 	
