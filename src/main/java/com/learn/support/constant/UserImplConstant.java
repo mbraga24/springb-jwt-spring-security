adcf1a45b8f747c6d9838ca7e84195bcdfd1e133
mrdbrg <rdg.brg@gmail.com>
2022-02-26 21:54:14 -0500

    class constant for UserServiceImpl class

---
+++ src/main/java/com/learn/support/constant/UserImplConstant.java
@@ -0,0 +1,9 @@
+package com.learn.support.constant;
+
+public class UserImplConstant {
+	public static final String EMAIL_ALREADY_EXIST = "Email already exist";
+	public static final String USERNAME_ALREADY_EXISTS = "Username already exists";
+	public static final String NO_USER_FOUND_BY_USERNAME = "User not found by username: ";
+	public static final String RETURNING_FOUND_USER_BY_USERNAME = "Returning found user by username: ";	
+	public static final String DEFAULT_USER_IMAGE_PROFILE = "/user/image/profile/temp";
+}
