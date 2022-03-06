09320b8c1343aad7ac3d633f2c9f3cfdd34948e1
mrdbrg <rdg.brg@gmail.com>
2022-03-05 19:32:54 -0500

    delete DEFAULT_USER_IMAGE_PROFILE constant

--- src/main/java/com/learn/support/constant/UserImplConstant.java
+++ src/main/java/com/learn/support/constant/UserImplConstant.java
@@ -5,5 +5,4 @@ public class UserImplConstant {
 	public static final String USERNAME_ALREADY_EXISTS = "Username already exists";
 	public static final String NO_USER_FOUND_BY_USERNAME = "User not found by username: ";
 	public static final String RETURNING_FOUND_USER_BY_USERNAME = "Returning found user by username: ";	
-	public static final String DEFAULT_USER_IMAGE_PROFILE = "/user/image/profile/temp";
 }
