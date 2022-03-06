995a6e585db04022de29678ff70227f17f833379
mrdbrg <rdg.brg@gmail.com>
2022-03-05 21:07:37 -0500

    add NO_USER_FOUND_BY_EMAIL constant

--- src/main/java/com/learn/support/constant/UserImplConstant.java
+++ src/main/java/com/learn/support/constant/UserImplConstant.java
@@ -4,5 +4,6 @@ public class UserImplConstant {
 	public static final String EMAIL_ALREADY_EXIST = "Email already exist";
 	public static final String USERNAME_ALREADY_EXISTS = "Username already exists";
 	public static final String NO_USER_FOUND_BY_USERNAME = "User not found by username: ";
-	public static final String RETURNING_FOUND_USER_BY_USERNAME = "Returning found user by username: ";	
+	public static final String RETURNING_FOUND_USER_BY_USERNAME = "Returning found user by username: ";
+	public static final String NO_USER_FOUND_BY_EMAIL = "User not found for email: ";	
 }
