6db7ab3f40dac01e6b963d84b7077c6931123ac1
mrdbrg <rdg.brg@gmail.com>
2022-03-05 19:35:45 -0500

    create file constants

---
+++ src/main/java/com/learn/support/constant/FileConstant.java
@@ -0,0 +1,21 @@
+package com.learn.support.constant;
+
+//=============================> FileConstant <=============================
+// This is a constant class where all constants will be public to be reused.
+//==========================================================================
+public class FileConstant {
+	public static final String USER_IMAGE_PATH = "/user/image/";
+	public static final String JPG_EXTENSION = "jpg";
+	public static final String USER_FOLDER = System.getProperty("user.home") + "/support/user/";
+	public static final String DIRECTORY_CREATED = "Created directory for: ";
+	public static final String DEFAULT_USER_IMAGE_PATH = "/user/image/profile/";
+	public static final String FILE_SAVED_IN_FILE_SYSTEM = "Saved file in file system by name: ";
+	public static final String DOT = ".";
+	public static final String FORWARD_SLASH = "/";
+	public static final String TEMP_PROFILE_IMAGE_BASE_URL = "https://randomuser.me/api/portraits/";
+	public static final String WOMEN = "women";
+	public static final String MEN = "men";
+	
+	// EXAMPLE URL:
+	// https://randomuser.me/api/portraits/women/12.jpg
+}
