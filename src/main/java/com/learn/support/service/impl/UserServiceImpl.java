078b83e6c2e1d41495a61bd0a90d48aae55134b0
mrdbrg <rdg.brg@gmail.com>
2022-03-05 19:36:49 -0500

    add methods

--- src/main/java/com/learn/support/service/impl/UserServiceImpl.java
+++ src/main/java/com/learn/support/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.transaction.Transactional;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -15,8 +16,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.learn.support.constant.FileConstant;
 import com.learn.support.constant.UserImplConstant;
 import com.learn.support.domain.User;
 import com.learn.support.domain.UserPrincipal;
@@ -49,8 +52,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 	}
 	
 	// The loadUserByUsername method gets called whenever Spring Security is trying to check the
-	// authentication of the user. The method needs to be overridden. 
-	
+	// authentication of the user. The method needs to be overridden. 	
 	@SuppressWarnings("static-access")
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -80,32 +82,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 			loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
 		}
 	}
-
-	@Override
-	public User register(String firstName, String lastName, 
-			String username, String email) throws UsernameExistException, UserNotFoundException, EmailExistException {
-		validateNewUsernameAndEmail(StringUtils.EMPTY, username, email);
-		User user = new User();
-		user.setUserId(generateUserId());
-		String password = generatePassword();
-		String encodedPassword = encodePassword(password);
-		user.setFirstName(firstName);
-		user.setLastName(lastName);
-		user.setUsername(username);
-		user.setEmail(email);
-		user.setJoinDate(new Date());
-		user.setPassword(encodedPassword);
-		user.setActive(true);
-		user.setNotLocked(true);
-		user.setRole(Role.ROLE_USER.name()); // setRole() takes a String. ROLE_USER is an enum and name() method will convert the enum into a String.
-		user.setAuthorities(Role.ROLE_USER.getAuthorities());
-		user.setProfileImageUrl(getTemporaryProfileImageUrl().toString());
-		userRepository.save(user); // call userRepository to save the user in the database
-		emailService.sendNewPasswordEmail(firstName, lastName, password, email);
-		// LOGGER.info("SEND A MESSAGE TO THE CONSOLE");
-		return user;
-	}
-
+	
 	// This method will be very generic to be applicable for when someone is either creating a new acccount 
 	// or updating their account.
 	@SuppressWarnings("static-access")
@@ -134,12 +111,86 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 			return null;
 		}
 	}
+
+	@Override
+	public User register(String firstName, String lastName, 
+			String username, String email) throws UsernameExistException, UserNotFoundException, EmailExistException {
+		validateNewUsernameAndEmail(StringUtils.EMPTY, username, email);
+		User user = new User();
+		user.setUserId(generateUserId());
+		String password = generatePassword();
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setUsername(username);
+		user.setEmail(email);
+		user.setJoinDate(new Date());
+		user.setPassword(encodePassword(password));
+		user.setActive(true);
+		user.setNotLocked(true);
+		user.setRole(Role.ROLE_USER.name()); // setRole() takes a String. ROLE_USER is an enum and name() method will convert the enum into a String.
+		user.setAuthorities(Role.ROLE_USER.getAuthorities());
+		user.setProfileImageUrl(getTemporaryProfileImageUrl().toString());
+		userRepository.save(user); // call userRepository to save the user in the database
+		emailService.sendNewPasswordEmail(firstName, lastName, password, email);
+		// LOGGER.info("SEND A MESSAGE TO THE CONSOLE");
+		return user;
+	}
 	
-	private Object getTemporaryProfileImageUrl() {
+	@Override
+	public User addNewUser(String firstName, String lastName, String username, String email, String role,
+			boolean isNonLocked, boolean isActive) {
+		validateNewUsernameAndEmail(StringUtils.EMPTY, username, email);
+		User user = new User();
+		user.setUserId(generateUserId());
+		String password = generatePassword();
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setUsername(username);
+		user.setEmail(email);
+		user.setJoinDate(new Date());
+		user.setPassword(encodePassword(password));
+		user.setActive(true);
+		user.setNotLocked(true);
+		user.setRole(getRoleEnumName(role).name());
+		user.setAuthorities(getRoleEnumName(role).getAuthorities());
+		user.setProfileImageUrl(getTemporaryProfileImageUrl(username).toString());
+		userRepository.save(user); 
+//		saveProfileImage(user, profileImage); // Not saving the image in the system unless all user's properties pass and user is saved in the database in userRepository.save(user).
+		emailService.sendNewPasswordEmail(firstName, lastName, password, email);
+		return user;
+	}
+
+	private Role getRoleEnumName(String role) {
+		return null;
+	}
+	
+	private void saveProfileImage(User user, MultipartFile profileImage) {
+	}
+
+	@Override
+	public User updateUser(String currentUsername, String firstName, String lastName, String username, String email,
+			String role, boolean isNonLocked, boolean isActive) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUser(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User updateProfileImage(String username, MultipartFile profileImage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private Object getTemporaryProfileImageUrl(String username) {
 		// ServletUriComponentsBuilder will return whatever the URL is for the service.
 		// e.g.: if the application is deployed to Google.com the based URL will be 
 		// www.google.com/...
-		return ServletUriComponentsBuilder.fromCurrentContextPath().path(UserImplConstant.DEFAULT_USER_IMAGE_PROFILE).toUriString();
+		return ServletUriComponentsBuilder.fromCurrentContextPath().path(FileConstant.DEFAULT_USER_IMAGE_PATH).toUriString();
 	}
 	
 	@Override
@@ -167,6 +218,11 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
 	private String generateUserId() {
 		return RandomStringUtils.randomNumeric(10);
+	}
+
+	@Override
+	public void resetPassword(String email) {
+		// TODO Auto-generated method stub
 	}
 
 }
