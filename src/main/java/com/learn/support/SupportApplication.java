ec724f5139d830a53531a17cd8a920e378605714
mrdbrg <rdg.brg@gmail.com>
2022-03-05 19:34:38 -0500

    creating user folders

--- src/main/java/com/learn/support/SupportApplication.java
+++ src/main/java/com/learn/support/SupportApplication.java
@@ -1,15 +1,20 @@
 package com.learn.support;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.learn.support.constant.FileConstant;
 
 @SpringBootApplication
 public class SupportApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupportApplication.class, args);
+		new File(FileConstant.USER_FOLDER).mkdirs();
 	}
 
 	@Bean
