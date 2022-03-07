550fb7ecef860edf1edd14f7785161a881213886
mrdbrg <rdg.brg@gmail.com>
2022-03-07 18:22:52 -0500

    comment typo

--- src/main/java/com/learn/support/enumeration/Role.java
+++ src/main/java/com/learn/support/enumeration/Role.java
@@ -3,8 +3,8 @@ package com.learn.support.enumeration;
 import com.learn.support.constant.Authority;
 
 //========================================> Role <========================================
-// This enum will be available to the rest of the application. When using one of the enums 
-// and getAuthorities() is called, it will return the authorities associated to that role.
+// This enum will be available to the rest of the application. When using one of the enums, 
+// getAuthorities() is called, it will return the authorities associated to that role.
 // This way if the a role is passed to a user, I can get the authorities for that user.
 //========================================================================================
 
