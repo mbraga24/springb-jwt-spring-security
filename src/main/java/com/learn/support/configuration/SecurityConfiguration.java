2b2b935c20fc04dd46a0515fc9a119a9a4c6b352
mrdbrg <rdg.brg@gmail.com>
2022-03-07 19:08:42 -0500

    refactoring comments

--- src/main/java/com/learn/support/configuration/SecurityConfiguration.java
+++ src/main/java/com/learn/support/configuration/SecurityConfiguration.java
@@ -31,9 +31,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	private UserDetailsService userDetailsService;
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
-	// I'm flagging SecurityConfiguration class which UserDetailsService is being used. 
-	// In this case, the annotation @Qualifier("UserDetailsService") refers to UserServiceImpl 
-	// class and not the default UserDetailsService
+	/**
+	 * I'm flagging SecurityConfiguration class which UserDetailsService is being used. 
+	 * In this case, the annotation @Qualifier("UserDetailsService") refers to UserServiceImpl 
+	 * class and not the default UserDetailsService.
+	 */
 	@Autowired
 	public SecurityConfiguration(JwtAuthorizationFilter jwtAuthorizationFilter,
 			JwtAccessDeniedHandler jwtAccessDeniedHandler, JwtAuthenticationEntryPoint jwtAuthenticationEntreyPoint,
@@ -46,24 +48,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
-    // ========================================================================================
-	// There are two methods from the WebSecurityConfigurerAdapter that I need to override 
-	// to create the security configuration in the application.
-    // ========================================================================================
-	
+	/**
+    * ========================================================================================
+	* There are two methods from the WebSecurityConfigurerAdapter that I need to override 
+	* to create the security configuration in the application.
+    * ========================================================================================
+	*/
+    
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// The AuthenticationManagerBuilder need to tell which UserDetailsService the application is using. 
-		// Whether it's the DAO (Data Access Object) that comes from the springframework.security.core package
-		// or the one annotated as UserServiceImpl class.
+		/**
+		* The AuthenticationManagerBuilder need to tell which UserDetailsService the application is using. 
+		* Whether it's the DAO (Data Access Object) that comes from the springframework.security.core package
+		* or the one annotated as UserServiceImpl class.
+		*/
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// Pass all the information that Spring Security needs to manage endpoint or url. 
-		// e.g.: which url is secure, which one is open, session management, filters and handlers.
-		
+		/**
+		* Pass all the information that Spring Security needs to manage endpoint or url. 
+		* e.g.: which url is secure, which one is open, session management, filters and handlers.
+		*/
 		http.csrf().disable().cors() // disable CSRF (cross site request forgery) ; add CORS (cross origin resource sharing) anyone that tries to access the application with a domain that hasn't been specified will be rejected
 		.and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // specify session management as STATELESS. the app will not use session to keep track of users, but will use JWT instead by a token that will be provided and will check if the user has it. 
@@ -78,7 +85,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	}
 	
 	// Bring a bean for the authentication manager
-	
 	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
