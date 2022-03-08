ba7062558fdfe05cd15288b389aff1a2fbae53f3
mrdbrg <rdg.brg@gmail.com>
2022-03-07 19:04:28 -0500

    custom httpStatus response method

--- src/main/java/com/learn/support/resource/UserResource.java
+++ src/main/java/com/learn/support/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.learn.support.resource;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.learn.support.constant.SecurityConstant;
 import com.learn.support.domain.HttpResponse;
@@ -117,11 +119,27 @@ public class UserResource extends ExceptionHandling {
 	@PreAuthorize("hasAuthority('user:delete')") // For someone to be able to delete an user, they will have to have the user:delete authority.
 	public ResponseEntity<HttpResponse> deleteUser(@PathVariable("id") long id) {
 		userService.deleteUser(id);
-		return response(HttpStatus.OK, USER_DELETED_SUCCESS);
+		return response(HttpStatus.NO_CONTENT, USER_DELETED_SUCCESS); // HttpStatus.NO_CONTENT -> (204, Series.SUCCESSFUL, "No Content")
 	}
 	
+	@PostMapping("/updateProfileImage")
+	public ResponseEntity<User> updateProfileImage(@RequestParam("username") String username, 
+			                                       @RequestParam(value = "profileImage") MultipartFile profileImage) throws UsernameExistException, UserNotFoundException, IOException, EmailExistException {
+		User updatedImageUser = userService.updateProfileImage(username, profileImage);
+		return new ResponseEntity<>(updatedImageUser, HttpStatus.OK);
+	}
+	
+	/**
+	 * Custom HttpResponse method - custom body to be returned.
+	 * @param httpStatus the httpStatus
+	 * @param message the custom message
+	 */
 	private ResponseEntity<HttpResponse> response(HttpStatus httpStatus, String message) {
-		return null;
+		// HttpResponse constructor: HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message)
+		HttpResponse body = new HttpResponse(httpStatus.value(), httpStatus, 
+				httpStatus.getReasonPhrase().toUpperCase(), message.toUpperCase());
+		// ResponseEntity(@Nullable T body, HttpStatus status): 
+		return new ResponseEntity<>(body, httpStatus); 
 	}
 
 	private void authenticateUser(String username, String password) {
